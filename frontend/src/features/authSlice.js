0946b5cf165f2b6d00eece40e68e1bc9799e219b
Himanshu <149322620+himanshu1081@users.noreply.github.com>
2025-08-30 20:49:51 +0530

    fix: Ensure secure uploads to Cloudinary and persist user data in local storage

--- frontend/src/features/authSlice.js
+++ frontend/src/features/authSlice.js
@@ -1,5 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const savedUser = JSON.parse(localStorage.getItem("userData"));
+
 const initialState = {
   isLoggedIn: false,
   userData: null,
@@ -12,10 +14,12 @@ const authSlice = createSlice({
     login: (state, action) => {
       state.isLoggedIn = true;
       state.userData = action.payload;
+      localStorage.setItem("userData", JSON.stringify(action.payload));
     },
     logout: (state) => {
       state.isLoggedIn = false;
       state.userData = null;
+      localStorage.removeItem("userData");
     },
   },
 });
