144ba8ad08abbe4befa0570b5e14dbeb09d6038c
Himanshu <149322620+himanshu1081@users.noreply.github.com>
2025-08-30 22:09:29 +0530

    fix: Remove localStorage handling from authSlice for user data management

--- frontend/src/features/authSlice.js
+++ frontend/src/features/authSlice.js
@@ -1,6 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const savedUser = JSON.parse(localStorage.getItem("userData"));
 
 const initialState = {
   isLoggedIn: false,
@@ -14,12 +13,10 @@ const authSlice = createSlice({
     login: (state, action) => {
       state.isLoggedIn = true;
       state.userData = action.payload;
-      localStorage.setItem("userData", JSON.stringify(action.payload));
     },
     logout: (state) => {
       state.isLoggedIn = false;
       state.userData = null;
-      localStorage.removeItem("userData");
     },
   },
 });
