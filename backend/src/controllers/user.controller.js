e6928ebe4c47c20c450181153ab3681f82d9b0f7
Himanshu <149322620+himanshu1081@users.noreply.github.com>
2025-09-06 15:33:18 +0530

    fix: Update cookie options in loginUser to improve security and set max age

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -92,8 +92,9 @@ const loginUser = asyncHandler(async (req, res) => {
         console.log(`Welcome back ${user.fullName}`)
         const option = {
             httpOnly: true,
-            sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+            sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
             secure: process.env.NODE_ENV === 'production',
+            maxAge: 24 * 60 * 60 * 1000
         };
         return res.status(200).cookie("accessToken", accessToken, option).cookie("refreshToken", refreshToken, option).json(
             new ApiResponse(200, { userData, accessToken, refreshToken }, "Cookies sent!")
